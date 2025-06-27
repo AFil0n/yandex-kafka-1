6b5211e0e1a5e0c0b2c984180d09d0c056c6e11c
Alexandr <lexfilonenko@yandex.ru>
2025-06-27 23:57:29 +0300

    автосборка зависимостей

---
+++ src/main/java/ru/yandex/kafka/KafkaLesson1Application.java
@@ -0,0 +1,13 @@
+package ru.yandex.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaLesson1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaLesson1Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/yandex/kafka/KafkaLesson1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yandex.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaLesson1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
